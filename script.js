a5f8a95a32dde4c45ec7404809cc13ae06a1b507
MikaelFM <89937771+MikaelFM@users.noreply.github.com>
2021-09-09 16:30:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+var x = 0;
+var hora = 0;
+var minuto = 0;
+var link = '';
+var d = '';
+function();
+document.getElementById('Pp2').innerHTML = d + ' - ' + hora + ':' + minuto; 
+const getDia = function(){
+        now = new Date;
+        var x = now.getDay();
+        var hora = now.getHours();
+        var minuto = now.getMinutes();
+        const ingles = 'https://meet.google.com/xdy-vzqw-mvm';
+        const sociologia = 'https://meet.google.com/mfb-ozep-qtn?hs=224';
+        const historia = 'https://meet.google.com/xks-fhea-rvo';
+        const matematica = 'https://meet.google.com/xbn-tyzt-dtr';
+        const dw = '';
+        const quimica = 'http://meet.google.com/fwb-yhjq-jbh';
+        const domingo = 0;
+        const segunda = 1;
+        const terca = 2;
+        const quarta = 3;
+        const quinta = 4;
+        const sexta = 5;
+        const sabado = 6;
+        if (x == domingo){
+        d = 'SEGUNDA-FEIRA'
+        window.alert("hoje não tem aula!")
+        } 
+        else if(x == segunda){
+            window.location.href = '#';
+            d = 'SEGUNDA-FEIRA';
+        }
+        else if(x == terca){
+            window.location.href = '#';
+            d = 'TERÇA-FEIRA';
+        }
+        else if(x == quarta){
+             d = 'QUARTA-FEIRA'
+            if (hora >= 15 && minuto > 10){
+                window.location.href = matematica
+            } else {
+                window.location.href = historia
+            }
+        }
+        else if(x == quinta){
+            d = 'QUINTA-FEIRA'
+            window.location.href = quimica
+        }
+        else if(x == sexta){
+             d = 'SEXTA-FEIRA'
+            window.location.href = '#'
+        }
+        else if(x == sabado){
+            window.alert("hoje não tem aula!");
+             d = 'SÁBADO'
+        }
+
+}
 function sleep(milliseconds){
     var start = new Date().getTime();
     var end=0;
@@ -18,46 +77,5 @@ if (c <= 100){
 }
 
 const redirecionar = function() {
-        now = new Date;
-        var x = now.getDay();
-        var hora = now.getHours();
-        var minuto = now.getMinutes();
-        const ingles = 'https://meet.google.com/xdy-vzqw-mvm';
-        const sociologia = 'https://meet.google.com/mfb-ozep-qtn?hs=224';
-        const historia = 'https://meet.google.com/xks-fhea-rvo';
-        const matematica = 'https://meet.google.com/xbn-tyzt-dtr';
-        const dw = '';
-        const quimica = 'http://meet.google.com/fwb-yhjq-jbh';
-        const domingo = 0;
-        const segunda = 1;
-        const terca = 2;
-        const quarta = 3;
-        const quinta = 4;
-        const sexta = 5;
-        const sabado = 6;
-        if (x == domingo){
-        window.alert("hoje não tem aula!")
-        } 
-        else if(x == segunda){
-            window.location.href = '#'
-        }
-        else if(x == terca){
-            window.location.href = '#'
-        }
-        else if(x == quarta){
-            if (hora >= 15 && minuto > 10){
-                window.location.href = matematica
-            } else {
-                window.location.href = historia
-            }
-        }
-        else if(x == quinta){
-            window.location.href = quimica
-        }
-        else if(x == sexta){
-            window.location.href = '#'
-        }
-        else if(x == sabado){
-            window.alert("hoje não tem aula!")
-        }
+        
 }
